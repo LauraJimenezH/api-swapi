b49297afbbf86b7fbfb6fddbd052789271a50fa4
MarinaRH <l_marina1717@hotmail.com>
2018-02-15 10:45:50 -0500

    agregando modal de datos

--- js/app.js
+++ js/app.js
@@ -25,3 +25,26 @@ function getFetch() {
       });
   }
 }
+
+//  modal con datos
+$(document).on('click', '.card', function() {
+  let el = $(this);
+  let name = document.getElementById('modal-title');
+  let gender = document.getElementById('modal-gender');
+  let height = document.getElementById('modal-height');
+  let mass = document.getElementById('modal-mass');
+  let birthday = document.getElementById('modal-bd');
+  let eye = document.getElementById('modal-eye');
+  let hair = document.getElementById('modal-hair');
+  let skin = document.getElementById('modal-skin');
+  let photo = document.getElementById('modal-photo');
+  mass.innerText = el.data('mass');
+  name.innerText = el.data('name');
+  gender.innerText = el.data('gender');
+  height.innerText = el.data('height');
+  birthday.innerText = el.data('bd');
+  eye.innerText = el.data('eye');
+  hair.innerText = el.data('hair');
+  skin.innerText = el.data('skin');
+  photo.setAttribute('src', el.data('url'));
+});
